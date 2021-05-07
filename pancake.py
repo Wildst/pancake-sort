a6428d2a67d464d291be0709c17bf0db16430b33
Lukas De Wilde <lukasdewilde0@gmail.com>
2021-05-07 22:40:55 +0200

    cleaning up

--- pancake.py
+++
@@ -1,7 +0,0 @@
-from tkinter import *
-from turtle import width
-
-class Pancake(Canvas):
-    def __init__(self, size, width, height, *args, **kwords):
-        Canvas.__init__(self, width = width,height=height, highlightthickness=0, bg="white", *args, **kwords)
-        self.size = size
